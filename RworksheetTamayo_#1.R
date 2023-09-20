e96a7fba6ff6a675448ae17e22d3b99c2726ddba
Kindrah <keandrahjade.tamayo@students.isatu.edu.ph>
2023-09-20 13:23:05 +0000

    first commit

---
+++ RworksheetTamayo_#1.R
@@ -0,0 +1,37 @@
+#Vector
+
+age <-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+        35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,
+        51, 35, 24, 33, 41.)
+
+#data points
+length (age)
+
+#reciprocal
+reciprocal <-1/age
+reciprocal
+
+#assign also new age <- c(age,0,age)
+new_Age <- c(34, 28, 14)
+new_Age
+
+#Sort the values for age
+sort_Age <-c(age)
+sort_Age
+
+#Minimum and Maximum
+min_Age <- c(age)
+min_Age
+max_Age <-c(age)
+max_Age 
+
+#Vector data
+data <-c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+         2.5, 2.3, 2.4, 2.7)
+
+#data points
+length (data)
+
+#Double the data
+double_Data <- c(data*2)
+double_Data
